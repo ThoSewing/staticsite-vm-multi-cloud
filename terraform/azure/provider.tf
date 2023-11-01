4559007657c8968000ee13556824a514d62fcb23
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-01 19:49:02 -0300

    Testando pipeline

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -14,8 +14,8 @@ terraform {
     # }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-staticsite-vm-tf"
-    storage_account_name = "staticsitevmkbtf001"
+    resource_group_name  = "rg-staticsite-vm-tf-thom01"
+    storage_account_name = "staticsitevmtstf001"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
