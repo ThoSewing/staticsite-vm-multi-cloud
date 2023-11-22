ba9139c21ae9c841b2299290e9b02fb9d9f6092c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-22 20:49:32 -0300

    Teste subindo infra AWS com LB

--- terraform/aws/modules/compute/vars.tf
+++ terraform/aws/modules/compute/vars.tf
@@ -15,7 +15,7 @@ variable "ami2" {
     type    = string
     default = "ami-02e136e904f3da870"
     validation {
-        condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
+        condition     = length(var.ami2) > 4 && substr(var.ami2, 0, 4) == "ami-"
         error_message = "The ami value must be a valid AMI id, starting with \"ami-\"."
     }
 }
