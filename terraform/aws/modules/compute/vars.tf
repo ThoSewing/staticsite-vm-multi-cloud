77c37b425f98026957d06ea4a9b29b6f7be466b3
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-22 20:46:27 -0300

    Teste subindo infra AWS com LB

--- terraform/aws/modules/compute/vars.tf
+++ terraform/aws/modules/compute/vars.tf
@@ -10,3 +10,47 @@ variable "ami" {
         error_message = "The ami value must be a valid AMI id, starting with \"ami-\"."
     }
 }
+
+variable "ami2" {
+    type    = string
+    default = "ami-02e136e904f3da870"
+    validation {
+        condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
+        error_message = "The ami value must be a valid AMI id, starting with \"ami-\"."
+    }
+}
+
+variable "ec2_lb_name" {
+    type    = string
+    default = "ec2-lb-name"
+}
+
+variable "ec2_lb_tg_name" {
+    type    = string
+    default = "ec2-lb-tg-name"
+}
+
+variable "ec2_lb_tg_protocol" {
+    type    = string
+    default = "HTTP"
+}
+
+variable "ec2_lb_tg_port" {
+    type    = number
+    default = 80
+}
+
+variable "ec2_lb_listener_protocol" {
+    type    = string
+    default = "HTTP"
+}
+
+variable "ec2_lb_listener_port" {
+    type    = number
+    default = 80
+}
+
+variable "ec2_lb_listener_action_type" {
+    type    = string
+    default = "forward"
+}
--- terraform/aws/modules/compute/vm.tf
+++ terraform/aws/modules/compute/vm.tf
@@ -52,3 +52,38 @@ resource "aws_instance" "instance" {
     vpc_security_group_ids = [aws_security_group.sg_public.id]
     user_data              = "${base64encode(data.template_file.cloud_init.rendered)}"
 }
+
+resource "aws_instance" "instance2" {
+    ami                    = "${var.ami2}"
+    instance_type          = "t2.micro"
+    subnet_id              = "${var.subnet_id}"
+    vpc_security_group_ids = [aws_security_group.sg_public.id]
+    user_data              = "${base64encode(data.template_file.cloud_init.rendered)}"
+}
+
+# RESOURCE: APPLICATION LOAD BALANCER
+
+resource "aws_lb" "ec2_lb" {
+    name               = "${var.ec2_lb_name}"
+    load_balancer_type = "application"
+    subnets            = ["${var.subnet_id}"]
+    security_groups    = [aws_security_group.sg_public.id]
+}
+
+resource "aws_lb_target_group" "ec2_lb_tg" {
+    name     = "${var.ec2_lb_tg_name}"
+    protocol = "${var.ec2_lb_tg_protocol}"
+    port     = "${var.ec2_lb_tg_port}"
+    vpc_id   = "${var.rede_id}"
+}
+
+resource "aws_lb_listener" "ec2_lb_listener" {
+    protocol          = "${var.ec2_lb_listener_protocol}"
+    port              = "${var.ec2_lb_listener_port}"
+    load_balancer_arn = aws_lb.ec2_lb.arn
+    
+    default_action {
+        type             = "${var.ec2_lb_listener_action_type}"
+        target_group_arn = aws_lb_target_group.ec2_lb_tg.arn
+    }
+}
