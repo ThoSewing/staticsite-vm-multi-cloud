7c37049d347524cd8ab8845424ac164e155c7ab7
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-22 21:07:20 -0300

    Teste subindo infra AWS com LB

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -2,12 +2,14 @@ module "rede" {
     source      = "./modules/rede"
     rede_cidr   = "${var.rede_cidr}"
     subnet_cidr = "${var.subnet_cidr}"
+    subnet2_cidr = "${var.subnet2_cidr}"
 }
 
 module "compute" {
     source     = "./modules/compute"
     rede_id    = "${module.rede.vpc_id}"
     subnet_id  = "${module.rede.subnet_id}"
+    subnet2_id = "${module.rede.subnet2_id}"
     rede_cidr  = "${var.rede_cidr}"
     ami        = "${var.ami}"
     depends_on = [module.rede]
