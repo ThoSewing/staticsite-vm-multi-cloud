f29eaf39d66f16adf72a4a6429f29738968376d6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-22 21:57:29 -0300

    Teste subindo infra AWS com LB (correcao)

--- terraform/aws/modules/compute/vm.tf
+++ terraform/aws/modules/compute/vm.tf
@@ -74,8 +74,19 @@ resource "aws_lb_target_group" "ec2_lb_tg" {
     name        = "${var.ec2_lb_tg_name}"
     protocol    = "${var.ec2_lb_tg_protocol}"
     port        = "${var.ec2_lb_tg_port}"
-    target_type = "alb"
     vpc_id      = "${var.rede_id}"
+}
+
+resource "aws_lb_target_group_attachment" "ec2_lb_tg-instance" {
+    target_group_arn = aws_lb_target_group.ec2_lb_tg.arn
+    target_id        = aws_instance.instance.id
+    port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "ec2_lb_tg-instance2" {
+    target_group_arn = aws_lb_target_group.ec2_lb_tg.arn
+    target_id        = aws_instance.instance2.id
+    port             = 80
 }
 
 resource "aws_lb_listener" "ec2_lb_listener" {
